fc3c91b22e58ecc50cc047491a7bef389c75e0a6
limingtong <limingtong@gmail.com>
2017-01-06 13:21:52 +0800

    Initial Commit

---
+++ MTFloridEyes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MTFloridEyes
+//
+//  Created by 明桐的Mac on 17/1/6.
+//  Copyright © 2017年 小谩的Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MTFloridEyes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MTFloridEyes
+//
+//  Created by 明桐的Mac on 17/1/6.
+//  Copyright © 2017年 小谩的Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
